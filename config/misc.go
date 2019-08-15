d80dac4382b1ee599f0a0c5fbab7092ceb4ad163
xalanq <xalanq@gmail.com>
2019-08-15 12:34:43 +0800

    Fix typo

--- config/misc.go
+++ config/misc.go
@@ -6,6 +6,6 @@ import (
 
 // SetGenAfterParse set it yes or no
 func (c *Config) SetGenAfterParse() (err error) {
-	c.GenAfterParse = util.YesOrNo(`Run "cf gen" after "cf test" (y/n)? `)
+	c.GenAfterParse = util.YesOrNo(`Run "cf gen" after "cf parse" (y/n)? `)
 	return c.save()
 }
