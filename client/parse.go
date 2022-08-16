762f8a5d3ef327b780e40bba29853a4dd712b139
Woshiluo Luo <woshiluo.luo@outlook.com>
2022-08-16 08:47:31 +0800

    fix: wrong sample input when the origin has color block

--- client/parse.go
+++ client/parse.go
@@ -26,9 +26,11 @@ func findSample(body []byte) (input [][]byte, output [][]byte, err error) {
 	if a == nil || b == nil || len(a) != len(b) {
 		return nil, nil, fmt.Errorf("Cannot parse sample with input %v and output %v", len(a), len(b))
 	}
-	newline := regexp.MustCompile(`<[\s/br]+?>`)
+	startline := regexp.MustCompile(`<[\s\S.]+?>`)
+	endline := regexp.MustCompile(`</[\s\S.]+?>`)
 	filter := func(src []byte) []byte {
-		src = newline.ReplaceAll(src, []byte("\n"))
+		src = endline.ReplaceAll(src, []byte("\n"))
+		src = startline.ReplaceAll(src, []byte(""))
 		s := html.UnescapeString(string(src))
 		return []byte(strings.TrimSpace(s) + "\n")
 	}
